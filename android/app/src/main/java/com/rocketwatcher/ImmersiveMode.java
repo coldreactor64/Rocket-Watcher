79cda8e1f7109005e45d93668e1ac9f3b7a4b734
Jeremy <jeremy@iMac.local>
2019-07-18 18:24:49 -0500

    Add no nav bar bottom

---
+++ android/app/src/main/java/com/rocketwatcher/ImmersiveMode.java
@@ -0,0 +1,6 @@
+package com.rocketwatcher;
+
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+public class ImmersiveMode extends ReactContextBaseJavaModule {
+}
---
+++ android/app/src/main/java/com/rocketwatcher/ImmersiveModePackage.java
@@ -0,0 +1,4 @@
+package com.rocketwatcher;
+
+public class ImmersiveModePackage {
+}
