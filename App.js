d46107d1e2daefc4e468e6acd6dd767e7bc986a3
Jeremy Hall <Coldreactor64@gmail.com>
2019-12-31 10:38:48 -0600

    Release Update

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native'
-import {Provider} from 'react-redux'
+import React, {useEffect, useState}from 'react';
+import {SafeAreaView, View} from 'react-native'
+import {Provider, useDispatch} from 'react-redux'
 import store from './redux/store'
 import AppNavigator from './navigation/AppNavigator';
 import NavigationBar from 'react-native-navbar-color'
+import { updateNews, loadMoreNews } from './redux/actions/newsActions';
+import { updateLaunches, loadMoreLaunches } from './redux/actions/launchesActions';
+
+import SplashScreen from 'react-native-splash-screen'
 
 export default class App extends React.Component {
   constructor() {
@@ -11,19 +15,31 @@ export default class App extends React.Component {
     this.state = {};
   }
 
-  componentDidMount(){
-    NavigationBar.setStatusBarColor('#000000',false)
-    NavigationBar.setStatusBarTheme('light',true)
-    NavigationBar.setColor('#000000')
-  }
   render() {
     return (
-    <SafeAreaView style = {{flex: 1}}>
-      <Provider store={store()}>
-        <AppNavigator/>
-      </Provider>
-    </SafeAreaView>
+        <Provider store={store()}>
+          <StartupElement/>
+        </Provider>
     );
   }
+}
+
+const StartupElement = () => {
+  const dispatch = useDispatch();
+  
+  useEffect(() => { //On mount, update the current launch
+    NavigationBar.setStatusBarColor('#000000',false);
+    NavigationBar.setStatusBarTheme('light', true);
+    NavigationBar.setColor('#000000');
+    dispatch(updateLaunches);
+    dispatch(updateNews);
+    SplashScreen.hide();
+  }, [dispatch]);
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+            <AppNavigator />
+    </SafeAreaView>
+  );
 }
 
